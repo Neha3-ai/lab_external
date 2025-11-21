5f5e9a44e5b4fe3e386869da3e29ade11db8dacf
Neha3-ai <mahankalineha3@gmail.com>
2025-11-21 10:35:10 +0530

    commiting

---
+++ Jenkins_java.java
@@ -0,0 +1,10 @@
+public class Jenkins_java
+{
+public static void main (String args[])
+{
+System.out.println("Hello World!");
+int a = 6;
+int b = 4;
+System.out.println("Sum of "+a+" and "+b+" is : "+sum);
+}
+}
